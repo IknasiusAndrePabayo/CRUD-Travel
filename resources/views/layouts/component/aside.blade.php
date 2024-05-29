372e575ccc75aa13d7c3252fc5e60eaafc49980a
IknasiusAndrePabayo <160708200+IknasiusAndrePabayo@users.noreply.github.com>
2024-05-29 19:42:46 +0700

    Update aside.blade.php

--- resources/views/layouts/component/aside.blade.php
+++ resources/views/layouts/component/aside.blade.php
@@ -1,7 +1,7 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar elevation-4" style="background-color: #006400;">
   <!-- Brand Logo -->
   <a href="#" class="brand-link">
-    <img src="" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <img src="" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">TravelQyta</span>
   </a>
 
