e57541029586b5c786aa43b4ea5f4675c0c6ebef
IknasiusAndrePabayo <160708200+IknasiusAndrePabayo@users.noreply.github.com>
2024-05-29 19:37:29 +0700

    1

--- resources/views/layouts/component/aside.blade.php
+++ resources/views/layouts/component/aside.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
   <a href="#" class="brand-link">
-    <img src="{{ asset('assets/dist/img/LOGO.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <img src="" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">TravelQyta</span>
   </a>
 
